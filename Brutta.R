e9156f410bde17dd997050aa17406570b9df38c7
Alex Fulgosi <alex.fulgosi@yahoo.it>
2018-08-23 16:12:52 +0200

    quasi terminata analisi costo

--- Brutta.R
+++ Brutta.R
@@ -61,12 +61,12 @@ ggplot(rfreq, aes(x = freq_sinistri, y = regione)) +
   geom_point(size=2) +
   geom_point(data=filter(rfreq, freq_sinistri<=0.06),color="blue",size=3) +
   geom_point(data=filter(rfreq, freq_sinistri>=0.1),color="red",size=3) +
-  geom_text(aes(label=regione), alpha=0.3)
+  geom_text(aes(label=regione), alpha=0.3) +
   labs(
     title = "Probabilità di sinistro per regione italiana",
     subtitle= "Quali regioni sono le 'peggiori' e quali le 'migliori'",
     x = "Probabilità di sinistro",      
-    y = "Regioni") +
+    y = "Regioni") 
 
 #questa differenza varrà anche a livelo provinciale? Vediamolo solo per le regioni più critiche
 #(questo poi sarà per dire che lo si fa a livello provinciale, ma farlo con tutte le prov era troppo messy)
@@ -116,7 +116,7 @@ ggplot(efreq, aes(eta, freq_sinistri)) +
   labs(
     title = "Probabilità di sinistro",
     subtitle = "Trend in base all'aumento dell'età del guidatore", 
-    x = "Età",
+    x = "Età del guidatore",
     y = "Probabilità di sinistro",
     fill = "Probabilità di sinistro") +
   scale_x_continuous(breaks = seq(20, 90, by = 10)) +
@@ -151,7 +151,7 @@ ggplot(hfreq, aes(HP_fiscali, freq_sinistri)) +
   geom_hline(aes(yintercept=mean(pol$num_sinistri)), linetype=2, alpha=0.5)
 
 #dai veicoli meno potenti ai veicoli fino a 2500 di cilindrata il trend va salendo
-#nei veicoli più potenti (oltre i 3000) sembra scendere, c'è poi un picco in quelli potentissimi (circa 4000)
+#nei veicoli più potenti (oltre i 3000) sembra scendere, ma aumenta la variabilità
 #___________________________________________________________________________________
 
 #con l'età del veicolo
@@ -207,7 +207,7 @@ ggplot(afreq, aes(alimentazione, freq_sinistri, fill=freq_sinistri)) +
 #___________________________________________________________________________________
 #Iniziamo ora a considerare il costo dei sinistri
 #_______________________________________________________________________________________
-
+#REGIONI
 
 #vediamo i boxplot per le regioni
 
@@ -229,11 +229,132 @@ ggplot(sinis2, aes(regione, costo)) +
 #in piemonte si registrano al primo quartile costi un poò più bassi
 #in calabria grande variabilità
 #avevamo osservato già una variabilità tra la prob di sinistro nelle regioni calabresi
+#di fatti, in Calabria:
+Cal <- sinis %>%
+  filter(regione=="Cala")
 
+ggplot(Cal, aes(PV_targa, costo)) +
+  geom_boxplot(color="#993300", fill="#FFFFCC") +
+  coord_cartesian(ylim=c(0,26000)) +
+  labs(
+    title = "Ammontare del costo dei sinistri nelle province calabresi",
+    x = "Province",
+    y = "Costo del sinistro") 
 
+#Sembra in questo casso esserci una differenza significativa tra le province
+#si guardi ad esempio  tra Catanzaro e .., in cui i costi hanno alta variabilità e sono generalmente superiori
+#La Calabria era tra le regioni da favorire, ma forse non la provincia di..
 
+#vediamo in termini di costo medio
+rcost <- aggregate(sinis[, 8], list(regione=sinis$regione), mean) %>%
+  rename(costo_med=costo)
+
+ggplot(rcost, aes(x = costo_med, y = regione)) +
+  geom_point(size=2) +
+  geom_point(data=filter(rcost, costo_med<=3500),color="blue",size=3) +
+  geom_point(data=filter(rcost, costo_med>=4000),color="red",size=3) +
+  geom_text(aes(label=regione), alpha=0.3) +
+  labs(
+    title = "Costo medio del sinistro per regione italiana",
+    x = "Costo medio sinistro",      
+    y = "Regioni") 
+
+#la provincia di ... è probabilmente quella che fa alzare di così tanto il costo medio:
+Cal_cost <- aggregate(Cal[, 8], list(PV_targa=Cal$PV_targa), mean) %>%
+  rename(costo_med=costo)
+
+ggplot(Cal_cost, aes(x = costo_med, y = PV_targa)) +
+  geom_point(size=2) +
+  geom_point(data=filter(Cal_cost, costo_med<=3500),color="blue",size=3) +
+  geom_point(data=filter(Cal_cost, costo_med>=4000),color="red",size=3) +
+  geom_text(aes(label=PV_targa), alpha=0.3) +
+  scale_x_continuous(breaks = seq(2000, 10000, by = 1000)) +
+  labs(
+    title = "Costo medio del sinistro per provincia calabrese",
+    x = "Costo medio sinistro",      
+    y = "Province") 
+#proprio come pensato..
+#___________________________________________________________________________________
+
+#con età
+
+ecost <- aggregate(sinis[, 8], list(eta=sinis$eta), mean) %>%
+  rename(costo_med=costo) %>%
+  filter(eta < 90)
+
+ggplot(sinis, aes(eta, costo)) +
+  geom_line(data=ecost, aes(eta, costo_med), color="blue", size=1, alpha=0.3) +
+  geom_smooth(method = lm) +
+  labs(
+    title = "Andamento del costo dei sinistri al crescere dell'età del guidatore",
+    subtitle = "Paragone con il trend del costo medio",
+    x = "Età del guidatore",      
+    y = "Costo del sinistro") 
+
+#si è visto precedentemente che la sinistrosità decresce con l'età
+#anche il costo! bisogna quindi avere una verta prudenza nell'assicurare i giovani
+
+#__________________________________________________________________________________
+
+#con potenza
+
+hcost <- aggregate(sinis[, 8], list(HP_fiscali=sinis$HP_fiscali), mean) %>%
+  rename(costo_med=costo) %>%
+  filter(HP_fiscali < 28)
+
+sinis2 <- filter(sinis, HP_fiscali < 28 )
+  
+
+ggplot(sinis2, aes(HP_fiscali, costo)) +
+  geom_line(data=hcost, aes(HP_fiscali, costo_med), color="#FF6600", size=1, alpha=0.3) +
+  geom_smooth(method = lm, color="#FF6600") +
+  labs(
+    title = "Andamento del costo dei sinistri al crescere della cilindrata del veicolo",
+    subtitle = "Paragone con il trend del costo medio",
+    x = "Cilindrata / 100",      
+    y = "Costo del sinistro") 
+
+#in questo caso l'andamento è crescente, seppur una crescita non particolarmente forte
+#è comunque un elemento che aggiunto alla sinistrosita crescente fa propendere per una minor tariffazione dei veicoli a bassa cilindrata
+
+#_________________________________________________________________________________________
+
+#con alimentazione
+#per alcune abbiamo poche obs:
+ggplot(sinis, aes(alimentazione))+
+  geom_bar() +
+  coord_cartesian(ylim=c(0,200))
+#togliamo 02, 12, 19, P3, P4
+
+acost <- aggregate(sinis[, 8], list(alimentazione=sinis$alimentazione), mean) %>%
+  rename(costo_med=costo) %>%
+  filter (alimentazione != "02" & alimentazione != "12" & alimentazione != "19" & alimentazione != "P3" & alimentazione != "P4")
+
+ggplot(acost, aes(alimentazione, costo_med, fill=costo_med)) +
+  geom_col() +
+  coord_polar() +
+  theme_minimal() + 
+  scale_fill_gradient(low="#CCFFCC", high="#009900", limits=c(1800,9000)) +
+  scale_y_continuous(breaks = seq(10000, 10001, by = 1)) +
+  labs(
+    title = "Costo medio del sinistro per tipologia di alimentazione del veicolo",
+    x = "",      
+    y = "",
+    fill = "Costo medio sinistro")
+
+#ABBELISCI  IL GRAFICO FREQUENZE E FAI BOXPLOT PER COSTO NON MEDIO
+
+#_______________________________________________________________________________________
+#poi fai un grafico per alimentazione simile a quello su freq ma con costo medio
+#vedi se eventualmente farme un altro con alimentazione e costo (non medio)
+#poi prendi le alimentazioni con più e meno costo e fai il modelo eta vs costo diversificato per quele alimentazioni
+#magari diversificalo anche per le regioni , o magari con dopia legenda sia per regioni che alimentazione
 #____________________________________________________________________________________
 
 #vedi se fare la cosa dei ested e variare qualche .. vs .. per l'età
+
+#fai un modello e usa le cose di many models o parti prima
+
+#se riesci creati un target binario e fai il mod logitico per far vedere le cose di DM
 
 #__________________________________________________________________________________
